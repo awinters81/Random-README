d2a840cd2295c2ecee6c7780100ba6e751f4f41b
awinters81 <alexandria.winters81@gmail.com>
2023-01-15 14:07:45 -0500

    creating the questions

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
 // TODO: Include packages needed for this application
+// So I know that I need to use the Inquire package, and I think I'll have to use the generateMarkdown.js since it has what I need for
+//licenses.
+
+const inquirer = require('inquirer');
+const generateMarkdown = require ('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// What would be typical questions for a README? The project name, a description, whose account it belongs to, email, maybe dependencies since
+// I know that we have a package.json file.
+const questions = [
+    
+    {
+        type: 'input',
+        name: 'Project Name',
+        message: 'Name of your project?'
+      },
+
+      {
+        type: 'input',
+        name: 'Description',
+        message: 'Please describe your project.'
+      },
+
+      {
+        type: 'input',
+        name: 'GitHub Account',
+        message: "What is your GitHub user name?"
+      },
+
+      {
+        type: 'input',
+        name: 'Email',
+        message: 'What is your email address?'
+      },
+
+      {
+        type: 'input',
+        name: 'Dependencies',
+        message: 'What command should be run to install dependencies?',
+      },
+
+      {
+        type: 'input',
+        name: 'License',
+        message: 'What kind of license should your project have?',
+      }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
